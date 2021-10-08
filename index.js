f26a11837df9c3db0ed7f79eba0dd6ab99f1b3ae
christinemkhachen <christinemkhachen@gmail.com>
2021-10-08 15:45:33 -0400

    commit tree 271f15784868a4e94453231d639685cdd8a6264c

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
+//the array above has 9 elements, which are strings. aka "blah"
 
-const titleCased = () => {
-  return tutorials
-}
+let titleCased = (input) => {
+  return tutorials.map((line) => {
+    //the .split(a seperator string belongs here yo) method takes a string and seprates it into an array. It breaks the string into idividiaj elements
+    //example "what does the this keyword mean?.split()will return ["what, "does", etc...]
+    let words = line.split(" ");
+    let capitalizedWords = words.map(
+      //We can use bracket notation to grab the first letter
+      (word) => word[0].toUpperCase() + word.slice(1)
+    );
+
+    let result = capitalizedWords.join(" ");
+    return result;
+  });
+};
